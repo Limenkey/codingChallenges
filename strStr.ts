4818660c2aaf382cb288c53ad5b875fd774633cf
limenkey <renlimenki@gmail.com>
2023-03-22 01:11:27 +0300

    Solved Find the Index of the First Occurrence in a String

---
+++ strStr.ts
@@ -0,0 +1,21 @@
+/*
+  Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+function strStr(haystack: string, needle: string): number {
+  if (needle === '') {
+      return 0;
+  }
+
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+      let j = 0;
+      while (j < needle.length && haystack[i + j] === needle[j]) {
+          j++;
+      }
+      if (j === needle.length) {
+          return i;
+      }
+  }
+
+  return -1;
+}
